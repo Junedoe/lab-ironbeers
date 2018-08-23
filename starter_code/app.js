1c765c33831c4eb853f9e0638fe07cc75cc106fd
Ojuna Faust <ojunafaust@MacbookHotspot.local>
2018-08-23 10:22:45 +0200

    BER - Ojuna - Done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,18 +1,44 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, './public')));
+hbs.registerPartials(path.join(__dirname, './views/partials'));
 
-app.get('/', (req, res, next) => {
-  res.render('index');
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Ironbeers'
+    });
 });
-
-
+app.get('/beers', (req, res) => {
+    punkAPI
+        .getBeers()
+        .then(punkAPIBeers => {
+            let data = {
+                beersforView: punkAPIBeers
+            };
+            res.render('beers', data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+});
+app.get('/random-beer', (req, res) => {
+    punkAPI
+        .getRandom()
+        .then(beers => {
+            console.log(beers);
+            res.render('random-beer', {
+                beers
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        });
+});
 app.listen(3000);
